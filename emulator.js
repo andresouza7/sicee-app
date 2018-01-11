e6be59f720b6e064c5c653e4b8c0f543c1b5d1a4
Andre Souza <andresouza@Andres-MacBook-Air-2.local>
2018-01-11 14:22:59 -0300

    unstaged files

--- emulator.js
+++
@@ -1,5 +0,0 @@
-curl -H "Content-Type: application/json" -X POST -d '[{"device_id":"1","power":"86.4","voltage":"120","current":"0.72"},{"device_id":"2","power":"50","voltage":"120","current":"0.6"},{"device_id":"3","power":"500","voltage":"120","current":"2.3"},{"device_id":"4","power":"20","voltage":"120","current":"0.12"}]' http://localhost:3000/api/telemetry
-
-curl -H "Content-Type: application/json" -X POST -d '[{"device_id":"11223","power":"86.4","voltage":"120","current":"0.72"}]' http://localhost:3000/api/telemetry
-
-curl -H "Content-Type: application/json" -X POST -d 'Aviso de queda de tensão :)' http://localhost:8080/api/smsAlert
